60c5f0d807993c0fdbd5ff78870925243011393e
Koala3353 <keenebrigado@gmail.com>
2024-04-11 09:32:18 +0800

    - Edited index.html - Fixes and changes

--- assets/js/logincheck.js
+++ assets/js/logincheck.js
@@ -1,4 +1,4 @@
-if(document.readyState !== 'loading') {
+function checkLoginStatus() {
     try {
         var blocker = document.querySelector('.loginrequired');
 
@@ -39,47 +39,10 @@ if(document.readyState !== 'loading') {
     } catch (error) {
         console.error('An error occurred:', error);
     }
+}
+
+if(document.readyState !== 'loading') {
+    checkLoginStatus();
 } else {
-    window.addEventListener("DOMContentLoaded", () => {
-        try {
-            var blocker = document.querySelector('.loginrequired');
-
-            console.log(localStorage);
-            var reflowAuth = localStorage.getItem('reflowAuth');
-            console.log(reflowAuth);
-
-            if (!reflowAuth) {
-                console.log('reflowAuth: Not logged in');
-                blocker.classList.add('active');
-            } else {
-                blocker.classList.remove('active');
-            }
-
-            blocker.addEventListener("click", function (event) {
-                try {
-                    console.log('button clicked');
-                    console.log(localStorage);
-                    var reflowAuth = localStorage.getItem('reflowAuth');
-                    console.log(reflowAuth);
-
-                    if (!reflowAuth) {
-                        event.preventDefault(); // Prevent default navigation
-                        console.log('reflowAuth: Not logged in');
-
-                        // Get the toast element
-                        var notification = document.querySelector('.notification');
-                        notification.style.display = 'block'; // or 'flex' if you're using a flex layout
-                        // Hide the notification after 3 seconds
-                        setTimeout(function () {
-                            notification.style.display = 'none';
-                        }, 3000);
-                    }
-                } catch (error) {
-                    console.error('An error occurred in the click event handler:', error);
-                }
-            });
-        } catch (error) {
-            console.error('An error occurred:', error);
-        }
-    });
+    window.addEventListener("DOMContentLoaded", checkLoginStatus);
 }
