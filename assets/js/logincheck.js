52bc8959a4c859ba34bfae85d292db4b4c452d00
Koala3353 <keenebrigado@gmail.com>
2024-04-11 09:08:49 +0800

    - Edited index.html - Fixes and changes

--- assets/js/logincheck.js
+++ assets/js/logincheck.js
@@ -1,7 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
     try {
         var blocker = document.querySelector('.loginrequired');
-        var buttons = document.getElementsByClassName('loginrequired');
 
         console.log(localStorage);
         var reflowAuth = localStorage.getItem('reflowAuth');
@@ -14,31 +13,29 @@ window.addEventListener("DOMContentLoaded", () => {
             blocker.classList.remove('active');
         }
 
-        for (var i = 0; i < buttons.length; i++) {
-            buttons[i].addEventListener("click", function (event) {
-                try {
-                    console.log('button clicked');
-                    console.log(localStorage);
-                    var reflowAuth = localStorage.getItem('reflowAuth');
-                    console.log(reflowAuth);
+        blocker.addEventListener("click", function (event) {
+            try {
+                console.log('button clicked');
+                console.log(localStorage);
+                var reflowAuth = localStorage.getItem('reflowAuth');
+                console.log(reflowAuth);
 
-                    if (!reflowAuth) {
-                        event.preventDefault(); // Prevent default navigation
-                        console.log('reflowAuth: Not logged in');
+                if (!reflowAuth) {
+                    event.preventDefault(); // Prevent default navigation
+                    console.log('reflowAuth: Not logged in');
 
-                        // Get the toast element
-                        var notification = document.querySelector('.notification');
-                        notification.style.display = 'block'; // or 'flex' if you're using a flex layout
-                        // Hide the notification after 3 seconds
-                        setTimeout(function() {
-                            notification.style.display = 'none';
-                        }, 3000);
-                    }
-                } catch (error) {
-                    console.error('An error occurred in the click event handler:', error);
+                    // Get the toast element
+                    var notification = document.querySelector('.notification');
+                    notification.style.display = 'block'; // or 'flex' if you're using a flex layout
+                    // Hide the notification after 3 seconds
+                    setTimeout(function() {
+                        notification.style.display = 'none';
+                    }, 3000);
                 }
-            });
-        }
+            } catch (error) {
+                console.error('An error occurred in the click event handler:', error);
+            }
+        });
     } catch (error) {
         console.error('An error occurred:', error);
     }
