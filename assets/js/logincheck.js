7ed51839816de9e2d7edcb9953bb69091ec04b22
Koala3353 <keenebrigado@gmail.com>
2024-04-11 09:13:39 +0800

    - Edited index.html - Fixes and changes

--- assets/js/logincheck.js
+++ assets/js/logincheck.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
+if(document.readyState !== 'loading') {
     try {
         var blocker = document.querySelector('.loginrequired');
 
@@ -39,4 +39,47 @@ window.addEventListener("DOMContentLoaded", () => {
     } catch (error) {
         console.error('An error occurred:', error);
     }
-});
+} else {
+    window.addEventListener("DOMContentLoaded", () => {
+        try {
+            var blocker = document.querySelector('.loginrequired');
+
+            console.log(localStorage);
+            var reflowAuth = localStorage.getItem('reflowAuth');
+            console.log(reflowAuth);
+
+            if (!reflowAuth) {
+                console.log('reflowAuth: Not logged in');
+                blocker.classList.add('active');
+            } else {
+                blocker.classList.remove('active');
+            }
+
+            blocker.addEventListener("click", function (event) {
+                try {
+                    console.log('button clicked');
+                    console.log(localStorage);
+                    var reflowAuth = localStorage.getItem('reflowAuth');
+                    console.log(reflowAuth);
+
+                    if (!reflowAuth) {
+                        event.preventDefault(); // Prevent default navigation
+                        console.log('reflowAuth: Not logged in');
+
+                        // Get the toast element
+                        var notification = document.querySelector('.notification');
+                        notification.style.display = 'block'; // or 'flex' if you're using a flex layout
+                        // Hide the notification after 3 seconds
+                        setTimeout(function () {
+                            notification.style.display = 'none';
+                        }, 3000);
+                    }
+                } catch (error) {
+                    console.error('An error occurred in the click event handler:', error);
+                }
+            });
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
+    });
+}
